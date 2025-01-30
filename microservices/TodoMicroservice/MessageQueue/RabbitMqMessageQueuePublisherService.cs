538271d9f5ac39779387f224e3db28c8966d9fa3
QuickOrBeDead <boraakgn@gmail.com>
2025-01-30 16:33:23 +0300

    Move todo microservice to Architecture repo

---
+++ microservices/TodoMicroservice/MessageQueue/RabbitMqMessageQueuePublisherService.cs
@@ -0,0 +1,51 @@
+﻿namespace MessageQueue;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Text.Json;
+
+using MessageQueue.Events;
+
+using RabbitMQ.Client;
+
+public interface IMessageQueuePublisherService
+{
+    Task PublishMessage<TEvent>([DisallowNull] TEvent @event)
+        where TEvent : EventBase;
+}
+
+public class RabbitMqMessageQueuePublisherService : IMessageQueuePublisherService
+{
+    private readonly IRabbitMqConnection _connection;
+
+    public RabbitMqMessageQueuePublisherService(IRabbitMqConnection connection)
+    {
+        _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+    }
+
+    public async Task PublishMessage<TEvent>([DisallowNull] TEvent @event)
+        where TEvent : EventBase
+    {
+        if (@event == null)
+        {
+            throw new ArgumentNullException(nameof(@event));
+        }
+
+        await using var channel = await _connection.CreateChannel().ConfigureAwait(false);
+
+        var eventName = EventNameAttribute.GetEventName<TEvent>();
+        await channel.ExchangeDeclareAsync(eventName, "fanout", true, false, null).ConfigureAwait(false);
+
+        var properties = new BasicProperties
+                             {
+                                 Headers = new Dictionary<string, object> { { "EventName", eventName } }!
+                             };
+
+        await channel.BasicPublishAsync(
+            eventName,
+            string.Empty,
+            true,
+            properties,
+            Encoding.UTF8.GetBytes(JsonSerializer.Serialize(@event)));
+    }
+}
