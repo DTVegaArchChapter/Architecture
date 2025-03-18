02e11f2dfff1b3e394b6900f87477054c42f1135
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2025-03-18 23:02:07 +0300

    feat: Add goal update functionality and clean up settings

--- ddd/goal-management-system/src/GoalManager.UseCases/GoalManagement/UpdateGoal/UpdateGoalCommandHandler.cs
+++ ddd/goal-management-system/src/GoalManager.UseCases/GoalManagement/UpdateGoal/UpdateGoalCommandHandler.cs
@@ -4,9 +4,9 @@ using GoalManager.Core.GoalManagement;
 using GoalManager.Core.GoalManagement.Specifications;
 
 namespace GoalManager.UseCases.GoalManagement.UpdateGoal;
-internal sealed class UpdateGoalCommandHandler(IRepository<GoalSet> goalSetRepository) : ICommandHandler<UpdateGoalCommand, Result>
+internal sealed class UpdateGoalCommandHandler(IRepository<GoalSet> goalSetRepository) : ICommandHandler<UpdateGoalCommand, Result<(int GoalSetId, int GoalId)>>
 {
-  public async Task<Result> Handle(UpdateGoalCommand request, CancellationToken cancellationToken)
+  public async Task<Result<(int GoalSetId, int GoalId)>> Handle(UpdateGoalCommand request, CancellationToken cancellationToken)
   {
     var goalSet = await goalSetRepository.SingleOrDefaultAsync(new GoalSetWithGoalsByGoalSetIdSpec(request.GoalSetId), cancellationToken).ConfigureAwait(false);
 
@@ -15,7 +15,21 @@ internal sealed class UpdateGoalCommandHandler(IRepository<GoalSet> goalSetRepos
       return Result.Error($"Goal set not found for id: {request.GoalSetId}");
     }
 
-    var updateGoalResult = goalSet.UpdateGoal(request.GoalId, request.Title, request.GoalType, request.GoalValue);
+    var goal = goalSet.Goals.SingleOrDefault(x => x.Id == request.GoalId);
+
+    if (goal is null)
+    {
+      return Result.Error($"Goal not found for id: {request.GoalId}");
+    }
+
+    var goalValueResult = GoalValue.Create(goal.GoalValue.MinValue, goal.GoalValue.MidValue, goal.GoalValue.MaxValue, request.GoalValueType);
+
+    if (!goalValueResult.IsSuccess)
+    {
+      return goalValueResult.ToResult();
+    }
+
+    var updateGoalResult = goalSet.UpdateGoal(request.GoalId, request.Title, request.GoalType, goalValueResult.Value);
 
     if (!updateGoalResult.IsSuccess)
     {
@@ -24,6 +38,6 @@ internal sealed class UpdateGoalCommandHandler(IRepository<GoalSet> goalSetRepos
 
     await goalSetRepository.UpdateAsync(goalSet, cancellationToken).ConfigureAwait(false);
 
-    return Result.Success();
+    return (goalSet.Id, goal.Id);
   }
 }
